655a077e537a754526f702702f3e620f03d8c0be
Julian Aiup <julian.aiup@aajtech.com>
2018-07-31 17:40:08 -0300

    Add webpack configs, css modules

---
+++ client/src/containers/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import css from './App.scss';
+
+export default () => (
+    <p className={css.p}>
+        { 'This is my new react app' }
+    </p>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,85 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import CleanWebpackPlugin from 'clean-webpack-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const parentDir = path.join(__dirname, '../');
-const outputDirectory = `${parentDir}/client/dist/`;
+const parentDirectory = __dirname;
+const clientDirectory = path.join(parentDirectory, 'client');
+const serverDirectory = path.join(parentDirectory, 'server');
+const clientOutputDirectory = path.join(clientDirectory, 'dist');
+const serverOutputDirectory = path.join(serverDirectory, 'dist');
 
-export default {
-    entry: [
-        path.join(parentDir, '/client/index.js'),
-    ],
+const mode = 'development';
+
+const clientConfig = {
+    name: 'client',
+    entry: path.join(clientDirectory, 'index.jsx'),
+    mode,
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
+                include: [
+                    clientDirectory,
+                ],
                 loader: 'babel-loader',
             },
             {
-                test: /\.less$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader'],
+                test: /\.scss$/,
+                include: [
+                    clientDirectory,
+                ],
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    },
+                    'sass-loader',
+                ],
             },
         ],
     },
     output: {
-        path: `${parentDir}/client/dist`,
+        path: clientOutputDirectory,
         filename: 'bundle.js',
     },
-    devServer: {
-        contentBase: parentDir,
-        historyApiFallback: true,
-    },
     plugins: [
-        new CleanWebpackPlugin([outputDirectory]),
+        new CleanWebpackPlugin([clientOutputDirectory]),
         new HtmlWebpackPlugin({
-            template: './client/index.html',
-            favicon: './client/favicon.ico',
+            template: path.join(clientDirectory, 'index.html'),
+            favicon: path.join(clientDirectory, 'favicon.ico'),
         }),
     ],
+    resolve: {
+        extensions: ['*', '.webpack.js', '.web.js', '.js', '.json', '.jsx'],
+    },
 };
+
+const serverConfig = {
+    name: 'server',
+    target: 'node',
+    entry: path.join(serverDirectory, 'index.js'),
+    mode,
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                include: [
+                    serverDirectory,
+                ],
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    output: {
+        path: serverOutputDirectory,
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin([serverOutputDirectory]),
+    ],
+};
+
+module.exports = [serverConfig, clientConfig];
